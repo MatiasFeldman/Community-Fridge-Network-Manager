828c7fb3f6547ee974985c9ab8f5d46e79d9af9a
Matias Feldman <129459131+MatiasFeldman@users.noreply.github.com>
2024-07-01 18:24:11 -0300

    Cronjobs finalizado

--- src/main/java/ar/edu/utn/frba/dds/models/main/MainReportes.java
+++ src/main/java/ar/edu/utn/frba/dds/models/main/MainReportes.java
@@ -1,10 +1,12 @@
 package ar.edu.utn.frba.dds.models.main;
 
-
 import ar.edu.utn.frba.dds.models.entities.helpers.reportes.IGeneradorPDF;
 import ar.edu.utn.frba.dds.models.entities.helpers.reportes.PDFgenerator;
-import ar.edu.utn.frba.dds.models.entities.reportes.*;
+import ar.edu.utn.frba.dds.models.entities.reportes.GenerarReportesCronJob;
+import ar.edu.utn.frba.dds.models.entities.reportes.IReporte;
+import ar.edu.utn.frba.dds.models.entities.reportes.ReporteMock;
 import ar.edu.utn.frba.dds.models.repositories.humanos.HumanosRepository;
+import ar.edu.utn.frba.dds.models.repositories.humanos.dao.HumanosDAO;
 import ar.edu.utn.frba.dds.models.repositories.incidentes.imp.IncidentesRepository;
 import ar.edu.utn.frba.dds.models.repositories.personasVulnerables.PersonasVulnerablesRepository;
 import com.itextpdf.text.pdf.PdfException;
@@ -12,34 +14,48 @@ import com.itextpdf.text.pdf.PdfException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MainReportes {
+    GenerarReportesCronJob generarReportesCronJob;
 
-    public static void main(String[] args) throws PdfException {
-        String path = "";
-        List<IReporte> reportes = new ArrayList<>();
-        reportes.add(new ReporteMock("Reporte 1", "Este es el contenido del primer reporte."));
-        reportes.add(new ReporteMock("Reporte 2", "Este es el contenido del segundo reporte."));
-        reportes.add(new ReporteMock("Reporte 3", "Este es el contenido del tercer reporte."));
-
-        IGeneradorPDF pdfGenerator = new PDFgenerator();
-
-        pdfGenerator.generarPDF(reportes, path);
+    public MainReportes(GenerarReportesCronJob generarReportesCronJob) {
+        this.generarReportesCronJob = generarReportesCronJob;
     }
 
-    /*
-    public static void main(String[] args) throws PdfException {
-        String path = "";
-        IncidentesRepository incidentesRepository = new IncidentesRepository(); // Cambiar por el repositorio real
-        IncidentesRepository humanosRepository = new HumanosRepository(); // Cambiar por el repositorio real
-        PersonasVulnerablesRepository personasVulnerablesRepository = new PersonasVulnerablesRepository(); // Cambiar por el repositorio real
+    public void iniciarCronJob() {
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        long intervalo = 1; // Cada 7 días (una semana)
+        TimeUnit unit = TimeUnit.MINUTES;
 
-        List<IReporte> reportes = List.of(
-                new ReporteFallas(incidentesRepository),
-                new ReporteViandasDonadas(humanosRepository),
-                new ReporteMovimientoViandas(humanosRepository, personasVulnerablesRepository));
+        scheduler.scheduleAtFixedRate(generarReportesCronJob, 0, intervalo, unit);
+    }
+
+    public static void main(String[] args) {
+        String filePath = "";
+        IGeneradorPDF generadorPDF = new PDFgenerator();
+        IncidentesRepository incidentesRepository = new IncidentesRepository(); // Cambiar por el repositorio real
+        HumanosRepository humanosRepository = new HumanosRepository(null);  // Cambiar por el repositorio real
+        PersonasVulnerablesRepository personasVulnerablesRepository = new PersonasVulnerablesRepository(null);  // Cambiar por el repositorio real
+
+        GenerarReportesCronJob reportesCronJob = new GenerarReportesCronJob(generadorPDF, filePath, incidentesRepository, humanosRepository, personasVulnerablesRepository);
+
+        MainReportes main = new MainReportes(reportesCronJob);
+        main.iniciarCronJob();
+    }
+}
+
+    /* public static void main(String[] args) throws PdfException {
+        String path = "/Users/matifeldman/Documentos/DDS";
 
         IGeneradorPDF pdfGenerator = new PDFgenerator();
-        pdfGenerator.generarPDF(reportes, path);
+
+
+        GenerarReportesCronJob reportesCronJob = new GenerarReportesCronJob(pdfGenerator, path);
+
+        MainReportes main = new MainReportes(reportesCronJob);
+        main.iniciarCronJob();
     } */
-}
+
